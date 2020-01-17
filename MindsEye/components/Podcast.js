d92047c126384339addb76d8df355832a1c10c43
tjanoski <tjanoski@wustl.edu>
2020-01-16 21:54:42 -0600

    Podcast Component

---
+++ MindsEye/components/Podcast.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+// Requires Three Props:
+//      1. url: Thumbnail URL
+//      2. title: Title of Podcast
+//      3. author: Author of Podcast
+
+// Will need to update with click event
+export class Podcast extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                    <Image
+                        style={styles.image}
+                        source={{ uri: this.props.uri }}
+                    />
+                    <View style={styles.info}>
+                        <Text style={styles.title}>{this.props.title}</Text>
+                        <Text style={styles.author}>{this.props.author}</Text>
+                    </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        width: '100%',
+        height: 150,
+        backgroundColor: 'gray',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        borderRadius: 1,
+        borderWidth: 2,
+        borderColor: "black"
+    },
+    image: {
+        marginLeft: 20,
+        width: 100, 
+        height: 100
+    },
+    info: {
+        marginLeft: 30,
+        flexShrink: 1,
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    author: {
+        marginLeft: 10,
+        fontSize: 16,
+        fontWeight: "500"
+    }
+  });
+  
